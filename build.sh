8a99ed8793df6f20bbeff17fb4fe3513e29d2a54
John Doering <ghostlander@gmail.com>
2018-03-06 04:57:04 +0000

    -D_FORCE_INLINES Added
    Works around a memcpy() declaration on Ubuntu 16.04 LTS
    in case of CUDA < 8.0
    
    /usr/include/string.h: In function ‘void* __mempcpy_inline(void*, const void*, size_t)’:
    /usr/include/string.h:652:42: error: ‘memcpy’ was not declared in this scope
       return (char *) memcpy (__dest, __src, __n) + __n;

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ rm -f config.status
 
 FLAGS="-march=native -falign-functions=16 -falign-jumps=16 -falign-labels=16"
 
-env CUDA_CFLAGS="-O3 -Xcompiler -Wall" CFLAGS="-O2 -fomit-frame-pointer $FLAGS" \
+env CUDA_CFLAGS="-O3 -Xcompiler -Wall -D_FORCE_INLINES" CFLAGS="-O2 -fomit-frame-pointer $FLAGS" \
 CXXFLAGS="-O2 -fomit-frame-pointer -fno-stack-protector $FLAGS" ./configure
 
 make
